3e665d553a58a70a5a9de18b3069ede8c42c5f61
Will Young <wil.wyo@gmail.com>
2023-05-09 21:55:35 -0400

    Create server configs and scripts

---
+++ packages/aws-config/lib/ServerStack/tnp_limitless_6/status.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+players=$(mcrcon list | egrep -o '[0-9]' | head -1)
+
+if [ -f /var/g.coldstart ]; then
+  rm /var/g.coldstart
+  echo 'coldstart'
+elif [ $players -eq 0 ]; then
+  echo 'idle'
+else
+  echo "active $p"
+fi
+
